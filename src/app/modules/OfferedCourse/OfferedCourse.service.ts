4a8b2abf263d44cf1a5c05cf31a26a4fcc9fa8ab
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2024-12-17 12:36:00 +0600

    Make hasTimeConflict utility function added

--- src/app/modules/OfferedCourse/OfferedCourse.service.ts
+++ src/app/modules/OfferedCourse/OfferedCourse.service.ts
@@ -7,6 +7,7 @@ import { AcademicFaculty } from '../academicFaculty/academicFaculty.model';
 import { AcademicDepartment } from '../academicDepartment/academicDepartment.model';
 import { Course } from '../Course/course.model';
 import { Faculty } from '../Faculty/faculty.model';
+import { hasTimeConflict } from './OfferedCourse.utils';
 
 const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
   const {
@@ -90,7 +91,7 @@ const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
   const assignedSchedules = await OfferedCourse.find({
     semesterRegistration,
     faculty,
-    days: {$in: days}
+    days: { $in: days },
   }).select('days startTime endTime');
 
   const newSchedule = {
@@ -99,19 +100,12 @@ const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
     endTime,
   };
 
-  assignedSchedules.forEach(schedule => {
-    const existingStartTime = new Date(`1970-01-01T${schedule.startTime}`);
-    const existingEndTime = new Date(`1970-01-01T${schedule.endTime}`);
-    const newStartTime = new Date(`1970-01-01T${schedule.startTime}`);
-    const newEndTime = new Date(`1970-01-01T${schedule.endTime}`);
-
-    if(newStartTime < existingEndTime && newEndTime > existingStartTime){
-      throw new AppError(
-        httpStatus.CONFLICT,
-        'This faculty is not available at that time ! Choose other time or day',
-      );
-    }
-  })
+  if (hasTimeConflict(assignedSchedules, newSchedule)) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      'This faculty is not available at that time ! choose other time or day',
+    );
+  }
 
   const result = await OfferedCourse.create({ ...payload, academicSemester });
   return result;
