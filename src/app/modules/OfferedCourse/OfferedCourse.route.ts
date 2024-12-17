d581af321e39d912dd64638bcc31e971ec4c850c
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2024-12-17 17:35:56 +0600

    update offered courses

--- src/app/modules/OfferedCourse/OfferedCourse.route.ts
+++ src/app/modules/OfferedCourse/OfferedCourse.route.ts
@@ -5,10 +5,22 @@ import { OfferedCourseControllers } from './OfferedCourse.controller';
 
 const router = express.Router();
 
+router.get('/', OfferedCourseControllers.getAllOfferedCourses);
+
+router.get('/:id', OfferedCourseControllers.getSingleOfferedCourses);
+
+router.patch(
+  '/:id',
+  validateRequest(OfferedCourseValidations.updateOfferedCourseValidationSchema),
+  OfferedCourseControllers.updateOfferedCourse,
+);
+
 router.post(
   '/create-offered-course',
   validateRequest(OfferedCourseValidations.createOfferedCourseValidationSchema),
   OfferedCourseControllers.createOfferedCourse,
 );
+
+router.delete('/:id', OfferedCourseControllers.deleteOfferedCourseFromDB);
 
 export const offeredCourseRoutes = router;
