003f4404007ce3966f827a84b374b2f5218529cb
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2024-12-18 11:28:12 +0600

    Intro to JWT , create an accessToken.

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -3,34 +3,49 @@ import AppError from '../../errors/AppError';
 import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import config from '../../config';
 
 const loginUser = async (payload: TLoginUser) => {
   // checking if user is exist
-   const user = await User.isUserExistsByCustomId(payload.id)
+  const user = await User.isUserExistsByCustomId(payload.id);
 
   if (!user) {
     throw new AppError(httpStatus.NOT_FOUND, 'This user is not found');
   }
   // checking if the user is already deleted
-//   const isDeleted = isUserExists?.isDeleted
+  const isDeleted = user?.isDeleted;
 
-//   if(isDeleted){
-//     throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted !')
-//   }
-//   // checking if the user is blocked
-//   const userStatus = isUserExists?.status
+  if (isDeleted) {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted !');
+  }
+  // checking if the user is blocked
+  const userStatus = user?.status;
 
-//   if(userStatus === 'blocked'){
-//     throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked !')
-//   }
+  if (userStatus === 'blocked') {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked !');
+  }
 
   // checking if the password is correct
-  if(!await User.isPasswordMatched(payload?.password, user?.password))
+  if (!(await User.isPasswordMatched(payload?.password, user?.password)))
+    // access Granted: Send AccessToken , RefreshToken
+    throw new AppError(httpStatus.FORBIDDEN, 'Password do not Matched');
 
-  // access Granted: Send AccessToken , RefreshToken
-  throw new AppError(httpStatus.FORBIDDEN, 'Password do not Matched');
-  
-  return {};
+  // create token and sent to the client
+
+  const jwtPayload = {
+    userId: user,
+    role: user.role,
+  };
+
+  const accessToken = jwt.sign(jwtPayload, config.jwt_access_secret as string, {
+    expiresIn: '10d',
+  });
+
+  return {
+    accessToken,
+    needsPasswordChage: user?.needsPasswordChange,
+  };
 };
 
 export const AuthServices = {
